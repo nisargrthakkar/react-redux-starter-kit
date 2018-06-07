5543e7461a7a9e80af6e586863cff671e539f98f
Nisarg Thakkar <apple@Nisargs-MacBook-Pro.local>
2018-06-07 22:18:43 +0530

    fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import App from './modules/App';
-import React from 'react';
+import React from 'react'; //eslint-disable-line
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
