e786bb87f5ee2843c8c1036b6e4f3d0e0b15d8b3
Nisarg Thakkar <>
2025-02-23 13:05:02 -0800

    Test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './redux/store';
 import './css/style.css';
-//Testssssss
+//Test
 
 //<H1>Hello</h1>
 
